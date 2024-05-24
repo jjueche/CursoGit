2b7862d0c9617bae0eef1e91698b9087ebabe645
juribe <juribe@navisaf.com>
2024-05-24 17:21:27 -0500

    inicial

---
+++ CursoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("De que número desea la tabla de multiplicar?: ");
+int nro = Convert.ToInt32(Console.ReadKey());
+int resultado = 0;
+for (int i = 1;i <= 10;i++)
+{
+    resultado = i * nro;
+    Console.WriteLine(i.ToString() + " x " + nro.ToString() + " = " + resultado.ToString());
+}
+Console.ReadLine();
+
